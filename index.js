f648ef27097f5e0804855d9ef1d27dba2d6c5dd6
Robby Kagema <robbymunchkin001@gmail.com>
2026-04-17 09:45:23 +0300

    Refactor combineUsers function and update package name in package-lock.json

--- index.js
+++ index.js
@@ -4,3 +4,26 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require('datejs');
+
+function combineUsers(...args) {
+
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(arr => {
+    combinedObject.users.push(...arr);
+  });
+
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+
+module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
